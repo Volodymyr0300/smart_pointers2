482ccf3e4de54aa66966393cab1c06360830b484
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-11-14 22:39:06 -0600

    some_practice_with_box

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,65 @@
-use std::rc::Rc;
+use std::mem;
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy)]
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+// A Rectangle can be specified by where its top left and bottom right
+// corners are in space
+#[allow(dead_code)]
+struct Rectangle {
+    top_left: Point,
+    bottom_right: Point,
+}
+
+fn origin() -> Point {
+    Point { x: 0.0, y: 0.0 }
+}
+
+fn boxed_origin() -> Box<Point> {
+    // Allocate this point on the heap, and return a pointer to it
+    Box::new(Point { x: 0.0, y: 0.0 })
+}
 
 fn main() {
-    let rc_examples = "Rc examples".to_string();
-    {
-        println!("--- rc_a is created ---");
+    // (all the type annotations are superfluous)
+    // Stack allocated variables
+    let point: Point = origin();
+    let rectangle: Rectangle = Rectangle {
+        top_left: origin(),
+        bottom_right: Point { x: 3.0, y: -4.0 }
+    };
 
-        let rc_a: Rc<String> = Rc::new(rc_examples);
-        println!("Reference Count of rc_a: {}", Rc::strong_count(&rc_a));
+    // Heap allocated rectangle
+    let boxed_rectangle: Box<Rectangle> = Box::new(Rectangle {
+        top_left: origin(),
+        bottom_right: Point { x: 3.0, y: -4.0 },
+    });
 
-        {
-            println!("--- rc_a is cloned to rc_b ---");
+    // The output of functions can be boxed
+    let boxed_point: Box<Point> = Box::new(origin());
 
-            let rc_b: Rc<String> = Rc::clone(&rc_a);
-            println!("Reference Count of rc_b: {}", Rc::strong_count(&rc_b));
-            println!("Reference Count of rc_a: {}", Rc::strong_count(&rc_a));
+    // Double indirection
+    let box_in_a_box: Box<Box<Point>> = Box::new(boxed_origin());
 
-            // Two `Rc`s are equal if their inner values are equal
-            println!("rc_a and rc_b are equal: {}", rc_a.eq(&rc_b));
+    println!("Point occupies {} bytes on the stack",
+             mem::size_of_val(&point));
+    println!("Rectangle occupies {} bytes on the stack",
+             mem::size_of_val(&rectangle));
 
-            // We can use methods of a value directly
-            println!("Length of the value inside rc_a: {}", rc_a.len());
-            println!("Value of rc_b: {}", rc_b);
+    // box size == pointer size
+    println!("Boxed point occupies {} bytes on the stack",
+             mem::size_of_val(&boxed_point));
+    println!("Boxed rectangle occupies {} bytes on the stack",
+             mem::size_of_val(&boxed_rectangle));
+    println!("Boxed box occupies {} bytes on the stack",
+             mem::size_of_val(&box_in_a_box));
 
-            println!("--- rc_b is dropped out of scope ---");
-        }
-
-        println!("Reference Count of rc_a: {}", Rc::strong_count(&rc_a));
-
-        println!("--- rc_a is dropped out of scope ---");
-    }
-
-    // Error! `rc_examples` already moved into `rc_a`
-    // And when `rc_a` is dropped, `rc_examples` is dropped together
-    // println!("rc_examples: {}", rc_examples);
-    // TODO ^ Try uncommenting this line
+    // Copy the data contained in `boxed_point` into `unboxed_point`
+    let unboxed_point: Point = *boxed_point;
+    println!("Unboxed point occupies {} bytes on the stack",
+             mem::size_of_val(&unboxed_point));
 }
