bf06241645bcb04a73f433025411389fd222c58c
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-11-14 22:26:52 -0600

    some_practice_with_Rc

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,37 @@
-enum List {
-    Cons(i32, Rc<List>),
-    Nil,
-}
-
-use crate::List::{Cons, Nil};
 use std::rc::Rc;
 
 fn main() {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("Count after creating a: {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("Count after creating b: {}", Rc::strong_count(&a));
+    let rc_examples = "Rc examples".to_string();
     {
-        let c = Cons(4, Rc::clone(&a));
-        println!("Count after creating c: {}", Rc::strong_count(&a));
+        println!("--- rc_a is created ---");
+
+        let rc_a: Rc<String> = Rc::new(rc_examples);
+        println!("Reference Count of rc_a: {}", Rc::strong_count(&rc_a));
+
+        {
+            println!("--- rc_a is cloned to rc_b ---");
+
+            let rc_b: Rc<String> = Rc::clone(&rc_a);
+            println!("Reference Count of rc_b: {}", Rc::strong_count(&rc_b));
+            println!("Reference Count of rc_a: {}", Rc::strong_count(&rc_a));
+
+            // Two `Rc`s are equal if their inner values are equal
+            println!("rc_a and rc_b are equal: {}", rc_a.eq(&rc_b));
+
+            // We can use methods of a value directly
+            println!("Length of the value inside rc_a: {}", rc_a.len());
+            println!("Value of rc_b: {}", rc_b);
+
+            println!("--- rc_b is dropped out of scope ---");
+        }
+
+        println!("Reference Count of rc_a: {}", Rc::strong_count(&rc_a));
+
+        println!("--- rc_a is dropped out of scope ---");
     }
-    println!("Count after c goes out of scope: {}", Rc::strong_count(&a));
+
+    // Error! `rc_examples` already moved into `rc_a`
+    // And when `rc_a` is dropped, `rc_examples` is dropped together
+    // println!("rc_examples: {}", rc_examples);
+    // TODO ^ Try uncommenting this line
 }
