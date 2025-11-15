c551a62dead1812c5d2c956422e7d211ca65abc1
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-11-15 12:54:50 -0600

    Rc<T>_and_RefCell<T>

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,25 @@
-pub trait Messenger {
-    fn send(&self, msg: &str);
+
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
 }
 
-pub struct LimitTracker<'a, T: Messenger> {
-    messenger: &'a T,
-    value: usize,
-    max: usize,
-}
-
-impl<'a, T> LimitTracker<'a, T>
-where
-    T: Messenger,
-{
-    pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
-        LimitTracker {
-            messenger,
-            value: 0,
-            max,
-        }
-    }
-
-    pub fn set_value(&mut self, value: usize) {
-        self.value = value;
-
-        let percentage_of_max = self.value as f64 / self.max as f64;
-
-        if percentage_of_max >= 1.0 {
-            self.messenger.send("Error: You are over your quota!");
-        } else if percentage_of_max >= 0.9 {
-            self.messenger
-                .send("Urgent warning: You've used up over 90% of your quota!");
-        } else if percentage_of_max >= 0.75 {
-            self.messenger
-                .send("Warning: You've used up over 75% of your quota!");
-        }
-    }
-}
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
 
 fn main() {
-    
-}
+    let value = Rc::new(RefCell::new(5)); // value is a RefCell containing 5
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::cell::RefCell;
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil))); // a points to value 5
 
-    struct MockMessenger {
-        sent_messages: RefCell<Vec<String>>,
-    }
+    let b = Rc::new(Cons(Rc::new(RefCell::new(3)), Rc::clone(&a))); // b points to 3 -> a (5)
+    let c = Rc::new(Cons(Rc::new(RefCell::new(4)), Rc::clone(&a))); // c points to 4 -> a (5)
 
-    impl MockMessenger {
-        fn new() -> MockMessenger {
-            MockMessenger {
-                sent_messages: RefCell::new(vec![]),
-            }
-        }
-    }
+    *value.borrow_mut() += 10; // Modify the value inside the RefCell by adding 10 to it
 
-    impl Messenger for MockMessenger {
-        fn send(&self, message: &str) {
-            self.sent_messages.borrow_mut().push(String::from(message));
-        }
-    }
-
-    #[test]
-    fn it_sends_an_over_75_percent_warning_message() {
-        let mock_messenger = MockMessenger::new();
-        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
-
-        limit_tracker.set_value(80);
-
-        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
-    }
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }
